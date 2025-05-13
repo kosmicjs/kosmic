1bd2717d39fc40a45ab0bf77505f83dd798571db
Spencer Snyder <sasnyde2@gmail.com>
2025-05-12 21:54:47 -0400

    chore: remove comments

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,14 +1,12 @@
 import {Kysely, PostgresDialect} from 'kysely';
 import type {Database} from '../models/index.js';
 import logger from '../utils/logger.js';
-// import {TimestampsPlugin} from './plugins/timestamps.js';
 import {pool} from './pool.js';
 
 export const db = new Kysely<Database>({
   dialect: new PostgresDialect({
     pool,
   }),
-  // plugins: [new TimestampsPlugin()],
   log(event) {
     if (event.level === 'error') {
       logger.error({
