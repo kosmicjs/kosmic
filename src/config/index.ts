3743d9f82905a19d28eee6152881e28a1bf3cd6c
Spencer Snyder <sasnyde2@gmail.com>
2025-07-02 22:17:46 -0400

    refactor: split env and config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,83 +1,12 @@
-import process from 'node:process';
-import path from 'node:path';
-import dotenv from 'dotenv';
 import z from 'zod/v4';
 import {type PoolConfig} from 'pg';
 import type JSONTransport from 'nodemailer/lib/json-transport/index.js';
 import type SMTPTransport from 'nodemailer/lib/smtp-transport/index.js';
-
-declare global {
-  // eslint-disable-next-line @typescript-eslint/no-namespace
-  namespace NodeJS {
-    interface ProcessEnv extends z.infer<typeof envSchema> {
-      NODE_ENV: 'development' | 'production' | 'test';
-      KOSMIC_ENV: 'development' | 'production' | 'migration' | 'test';
-    }
-  }
-}
-
-const nodeEnv = z
-  .enum(['development', 'production', 'test'])
-  .default('development')
-  .parse(process.env.NODE_ENV);
-
-const kosmicEnv = z
-  .enum(['development', 'production', 'migration', 'test'])
-  .default('development')
-  .parse(process.env.KOSMIC_ENV);
-
-const parsedEnv: Record<string, string> = {};
-
-// laod .env first
-dotenv.config({
-  path: path.resolve(import.meta.dirname, process.cwd(), `.env`),
-  processEnv: parsedEnv,
-  quiet: true,
-});
-
-// then override with specific env
-dotenv.config({
-  path: path.resolve(import.meta.dirname, process.cwd(), `.env.${kosmicEnv}`),
-  processEnv: parsedEnv,
-  override: true,
-  quiet: true,
-});
-
-export const envSchema = z.object({
-  PORT: z.string().default('3000'),
-  SERVER_HOST: z.string().default('127.0.0.1'),
-  STRIPE_SECRET_KEY: z.string().optional(),
-  STRIPE_ENDPOINT_SECRET: z.string().optional(),
-  STRIPE_PRICE_ID: z.string().optional(),
-  DB_HOST: z.string().optional(),
-  DB_DATABASE: z.string().optional(),
-  DB_USER: z.string().optional(),
-  DB_PASSWORD: z.string().optional(),
-  DB_CONNECTION_STRING: z.string().optional(),
-  SESSION_KEYS: z.string().optional(),
-  LOG_LEVEL: z.string().default('info'),
-  OPENAI_API_KEY: z.string().optional(),
-  GEMINI_API_KEY: z.string().optional(),
-  GOOGLE_CLIENT_ID: z.string().optional(),
-  GOOGLE_CLIENT_SECRET: z.string().optional(),
-  GOOGLE_CALLBACK_URL: z.string().optional(),
-  GITHUB_CLIENT_ID: z.string().optional(),
-  GITHUB_CLIENT_SECRET: z.string().optional(),
-  GITHUB_CALLBACK_URL: z.string().optional(),
-  SMTP_HOST: z.string().optional(),
-  SMTP_PORT: z.string().optional(),
-  SMTP_USER: z.string().optional(),
-  SMTP_PASS: z.string().optional(),
-});
-
-// Variables set outside of the env files override the env files
-const env = envSchema.parse({
-  ...parsedEnv,
-  ...process.env,
-});
+import {env, nodeEnv, kosmicEnv} from './env.js';
 
 /**
  * The configuration schema for the application
+ * The schema is used to validate the configuration and set defaults
  */
 export const configSchema = z.object({
   kosmicEnv: z.literal(kosmicEnv).default(kosmicEnv),
@@ -150,6 +79,10 @@ export const configSchema = z.object({
   ).prefault({}),
 });
 
+/**
+ * If you have a specific configuration for each environment, you can define it here.
+ * The configuration will be merged with the defaults from the schema.
+ */
 const configByEnv = {
   development: {},
   production: {},
@@ -165,10 +98,4 @@ type Config = Omit<z.infer<typeof configSchema>, 'db' | 'nodeMailer'> & {
   nodeMailer: SMTPTransport.Options | JSONTransport.Options;
 };
 
-export const config: Config = (() => {
-  const parsedConfig: z.infer<typeof configSchema> = configSchema.parse(
-    configByEnv[nodeEnv],
-  );
-
-  return parsedConfig;
-})();
+export const config: Config = configSchema.parse(configByEnv[nodeEnv]);
